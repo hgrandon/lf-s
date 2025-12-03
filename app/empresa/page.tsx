1877e9703433830a52530a2437efbb3fd9536166
Hector Grandon <hgrandon@live.cl>
2025-12-03 20:38:12 -0300

    Cambiar Histórico a Empresa

---
+++ app/empresa/page.tsx
@@ -0,0 +1,7 @@
+export default function EmpresaPage() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-violet-800 via-fuchsia-700 to-indigo-800 text-white">
+      <h1 className="text-center pt-24 font-bold text-2xl">Empresa</h1>
+    </main>
+  );
+}
